847585fb45b4be2e4e2c8f4981fd4570e9a8d1a4
TheDailyKaylee <40675848+TheDailyKaylee@users.noreply.github.com>
2019-02-03 09:45:44 -0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function login() {
-var username = document.getElementById('username').value;
-var password =  document.getElementById('password').value;
-if ((username=='Klee'&& password=='wordsaremagic')||(username=='Mlee' && password=='magicmom')||(username=='Llee'&& password=='deftdad')||(username=='Dlaney'&& password=='hazelunicorn')||(username=='guest'&& password=='writing1')||(username=='jyen'&& password=='humanities102')||(username=='bwhipp'&& password=='humanities64')||(username=='ellabc23'&& password=='readingrules')){
-window.location.href='home.html';
-}
-} 
