8af7e4c89057c85d6c24309003e0166b647b731c
TheDailyKaylee <40675848+TheDailyKaylee@users.noreply.github.com>
2019-02-14 16:57:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
 if ((username=='Klee'&& password=='wordsaremagic')||(username=='Mlee' && password=='magicmom')||(username=='Llee'&& password=='deftdad')||(username=='Dlaney'&& password=='hazelunicorn')||(username=='guest'&& password=='writing1')){
-window.location.href='index.html';
+window.location.href='home.html';
 }
 } 
