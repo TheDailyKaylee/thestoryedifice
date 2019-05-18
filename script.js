9678967f7d4b27694614a6a48f5845e75e55b78d
TheDailyKaylee <40675848+TheDailyKaylee@users.noreply.github.com>
2019-05-18 10:28:04 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function login() {
-var username = document.getElementById('username').value;
-var password =  document.getElementById('password').value;
-if ((username=='Klee'&& password=='wordsaremagic')||(username=='ellabc23'&& password=='ihaveamembership')||(username=='guest'&& password=='writing1')){
-window.location.href='home.html';
-}
-} 
